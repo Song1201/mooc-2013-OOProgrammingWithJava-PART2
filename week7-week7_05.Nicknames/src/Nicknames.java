5c9412e0d350bc94eb4d9b76bd62af9bef1fa58c
Song1201 <songchenli1201@outlook.com>
2018-10-09 09:19:43 -0700

    Finished 05.Nicknames

--- week7-week7_05.Nicknames/src/Nicknames.java
+++ week7-week7_05.Nicknames/src/Nicknames.java
@@ -6,6 +6,11 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations requested in the assignment here!
+        HashMap<String,String> nickNames = new HashMap<String, String>();
+        nickNames.put("matti","mage");
+        nickNames.put("mikael","mixu");
+        nickNames.put("arto","arppa");
+        System.out.println(nickNames.get("mikael"));
     }
 
 }
