c469c0a34f72982886ac6146023c998f265ae221
Song1201 <songchenli1201@outlook.com>
2018-10-16 09:52:15 -0700

    Finished 19.MethodArgumentValidation

--- week9-week9_19.MethodArgumentValidation/src/validation/Person.java
+++ week9-week9_19.MethodArgumentValidation/src/validation/Person.java
@@ -2,20 +2,22 @@ package validation;
 
 public class Person {
 
-    private String name;
-    private int age;
+  private String name;
+  private int age;
 
-    public Person(String name, int age) {
-
-        this.name = name;
-        this.age = age;
+  public Person(String name, int age) {
+    if(name==null||name.length()==0||name.length()>40||age<0||age>120) {
+      throw new IllegalArgumentException("Please give valid input.");
     }
+    this.name = name;
+    this.age = age;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public int getAge() {
-        return age;
-    }
+  public int getAge() {
+    return age;
+  }
 }
