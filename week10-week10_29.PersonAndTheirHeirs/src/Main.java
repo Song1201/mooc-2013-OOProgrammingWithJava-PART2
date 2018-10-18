510f2ae52b379b5c97b43162f933605b07cdf990
Song1201 <songchenli1201@outlook.com>
2018-10-17 21:58:03 -0700

    Finished 29.PersonAndTheirHeirs

--- week10-week10_29.PersonAndTheirHeirs/src/Main.java
+++ week10-week10_29.PersonAndTheirHeirs/src/Main.java
@@ -1,8 +1,14 @@
-
+import people.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // test your code here
-    }
+  public static void main(String[] args) {
+    // test your code here
+  }
+
+  public static void printDepartment(List<Person> people) {
+    for(Person person:people) System.out.println(person);
+    
+  }
 }
