ab3632aa7b33999446cd8b2f5871fb785f84ec16
Song1201 <songchenli1201@outlook.com>
2018-10-20 14:17:41 -0700

    Finished 37.Survey

--- week11-week11_37.Survey/src/survey/UserInterface.java
+++ week11-week11_37.Survey/src/survey/UserInterface.java
@@ -11,6 +11,27 @@ public class UserInterface implements Runnable {
     @Override
     public void run() {
         // Create your app here
+        frame = new JFrame("Survey");
+        frame.setPreferredSize(new Dimension(200, 300));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        Container container = frame.getContentPane();
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        container.add(new JLabel("Are you?"));
+        container.add(new JCheckBox("Yes!"));
+        container.add(new JCheckBox("No!"));
+        container.add(new JLabel("Why?"));
+        JRadioButton noReason = new JRadioButton("No reason.");
+        JRadioButton fun = new JRadioButton("Because it is fun!");
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(noReason);
+        buttonGroup.add(fun);
+        container.add(noReason);
+        container.add(fun);
+        container.add(new JButton("Done!"));
+        
+        frame.pack();
+        frame.setVisible(true);
     }
 
 
