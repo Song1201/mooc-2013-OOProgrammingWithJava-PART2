523c39c8393eae8000c75d8ae8b381d2b712f62c
Song1201 <songchenli1201@outlook.com>
2018-10-20 21:00:57 -0700

    Finished 39.ClickEffect

--- week11-week11_39.ClickEffect/src/clicker/ui/Main.java
+++ week11-week11_39.ClickEffect/src/clicker/ui/Main.java
@@ -1,11 +1,14 @@
 package clicker.ui;
 
+import clicker.applicationlogic.Calculator;
+import clicker.applicationlogic.PersonalCalculator;
 import javax.swing.SwingUtilities;
 
 public class Main {
 
-    public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        SwingUtilities.invokeLater(ui);
-    }
+  public static void main(String[] args) {
+    Calculator calculator = new PersonalCalculator();
+    UserInterface ui = new UserInterface(calculator);
+    SwingUtilities.invokeLater(ui);
+  }
 }
--- week11-week11_39.ClickEffect/src/clicker/ui/UserInterface.java
+++ week11-week11_39.ClickEffect/src/clicker/ui/UserInterface.java
@@ -1,29 +1,43 @@
 package clicker.ui;
 
+import clicker.applicationlogic.Calculator;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class UserInterface implements Runnable {
-    private JFrame frame;
 
+  private JFrame frame;
+  private Calculator calculator;
 
-    @Override
-    public void run() {
-        frame = new JFrame("Click Effect");
-        frame.setPreferredSize(new Dimension(200, 100));
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+  public UserInterface(Calculator calculator) {
+    this.calculator = calculator;
+  }
+  
+  public void run() {
+    frame = new JFrame("Click Effect");
+    frame.setPreferredSize(new Dimension(200, 100));
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        createComponents(frame.getContentPane());
+    createComponents(frame.getContentPane());
 
-        frame.pack();
-        frame.setVisible(true);
-    }
+    frame.pack();
+    frame.setVisible(true);
+  }
 
-    private void createComponents(Container container) {
-    }
+  private void createComponents(Container container) {
+    JLabel showNumber = new JLabel("0");
+    JButton click = new JButton("Click!");
+    ActionListener clickToIncrease = new ClickListener(calculator, showNumber);
+    click.addActionListener(clickToIncrease);
+    container.setLayout(new BorderLayout());
+    container.add(showNumber,BorderLayout.WEST);
+    container.add(click,BorderLayout.SOUTH);
+  }
 
-    public JFrame getFrame() {
-        return frame;
-    }
+  public JFrame getFrame() {
+    return frame;
+  }
 }
