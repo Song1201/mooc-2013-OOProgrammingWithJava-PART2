12ee8b37e5a1f1737389b3ccb0bfbc6039bc86f7
Song1201 <songchenli1201@outlook.com>
2018-10-28 22:52:30 -0700

    Finished 44.RegularExpressions

--- week12-week12_44.RegularExpressions/src/Main.java
+++ week12-week12_44.RegularExpressions/src/Main.java
@@ -1,7 +1,24 @@
 
 public class Main {
 
-    public static void main(String[] args) {
-        // write test code here
+  public static void main(String[] args) {
+    // write test code here
+   
+  }
+  
+  public static boolean isAWeekDay(String string) {
+    return string.matches("mon|tue|wed|thu|fri|sat|sun");
+  }
+  
+  public static boolean allVowels(String string) {
+    return string.matches("(a|e|i|o|u|ä|ö)*");
+  }
+  
+  public static boolean clockTime(String string) {
+    if(string.matches("[0-9]{2}(:[0-5][0-9]){2}")) {
+      int hour = Integer.parseInt(string.substring(0, 2));
+      if(hour<24 && hour>=0) return true;
     }
+    return false;
+  }
 }
