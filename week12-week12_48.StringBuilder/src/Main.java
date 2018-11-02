58521a3fbeddf78f09592022dc3ae318dc118354
Song1201 <songchenli1201@outlook.com>
2018-11-01 19:12:34 -0700

    Finished 48.StringBuilder

--- week12-week12_48.StringBuilder/src/Main.java
+++ week12-week12_48.StringBuilder/src/Main.java
@@ -1,12 +1,21 @@
 
 public class Main {
 
-    public static void main(String[] args) {
-        int[] t = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(build(t));
-    }
+  public static void main(String[] args) {
+    int[] t = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    System.out.println(build(t));
+  }
 
-    public static String build(int[] t) {
-        return "{\n}\n";
+  public static String build(int[] t) {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append("{\n");
+    int LINE_LENGTH = 4;
+    for(int i=0;i<t.length-1;i++) {
+      stringBuilder.append(" " + t[i] + ",");
+      if((i+1)%LINE_LENGTH==0) stringBuilder.append("\n");
     }
+    stringBuilder.append(" ").append(t[t.length-1]).append("\n}\n");
+    
+    return stringBuilder.toString();
+  }
 }
